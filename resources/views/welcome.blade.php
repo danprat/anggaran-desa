e2bafce08466978c81883e1cd90b3a1bfd31a9a1
Dany Pratmanto <danybms@gmail.com>
2025-07-06 12:54:15 +0700

    fix: tambah favicon dinamis ke landing page dan welcome page
    - Tambah component <x-favicon /> ke landing.blade.php
    - Tambah component <x-favicon /> ke welcome.blade.php
    - Sekarang semua halaman (app, guest, landing, welcome) menggunakan favicon dinamis
    - Favicon otomatis berubah sesuai logo desa di semua halaman
    - Konsistensi branding di seluruh aplikasi

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,9 @@
 
         <title>Laravel</title>
 
+        <!-- Dynamic Favicon -->
+        <x-favicon />
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
