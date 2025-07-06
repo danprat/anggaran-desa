b0178412d20a1086e1f54d5a2e060e96e242db38
Dany Pratmanto <danybms@gmail.com>
2025-07-06 20:51:39 +0700

    fix: terapkan styling nav-link pada menu Admin dropdown
    - Ubah styling button Admin dari px-3 py-2 ke px-1 pt-1 border-b-2
    - Gunakan styling yang sama dengan menu Laporan dan menu navigasi lainnya
    - Menu Admin sekarang sejajar dan konsisten dengan menu navigasi lainnya
    - Semua submenu Admin tetap dipertahankan (Manajemen User, Tahun Anggaran, Profil Desa, Log Aktivitas)

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -44,7 +44,7 @@
                     @if(auth()->user()->can('manage-users') || auth()->user()->can('view-admin') || auth()->user()->can('view-desa-profile') || auth()->user()->can('view-log'))
                         <!-- Admin Dropdown -->
                         <div class="relative" x-data="{ open: false }">
-                            <button @click="open = ! open" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150" :class="{ 'text-gray-900 bg-gray-50': open }">
+                            <button @click="open = ! open" class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out" :class="{ 'text-gray-900 border-gray-300': open }">
                                 <div>{{ __('Admin') }}</div>
                                 <div class="ms-1">
                                     <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
